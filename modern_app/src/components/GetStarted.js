a5affacd29c0cf8924832e1fda1fd2d4d03d717a
priyanshu082 <priyanshusingh216@gmail.com>
2023-03-17 12:49:47 +0530

    nav+hero both completed

--- modern_app/src/components/GetStarted.js
+++ modern_app/src/components/GetStarted.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import styles from '../style'
+import { arrowUp } from '../assets'
 
-const GetStarted = () => {
+
+const  GetStarted = () => {
   return (
-    <div>GetStarted</div>
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[3px] cursor-pointer `}>
+      <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+       <div className={`${styles.flexCenter} flex-row`}>
+         <p className='font-poppins font-medium text-[18px] leading-[23px]'>
+          <span className='text-gradient mr-1'>Get</span>
+         </p>
+         <img src={arrowUp} alt="arrow" className='w-[23px] h-[23px]'/>
+       </div>
+       <p className='font-poppins font-medium text-[18px] leading-[23px]'>
+          <span className='text-gradient'>Started</span>
+         </p>
+      </div>
+    </div>
   )
 }
 
