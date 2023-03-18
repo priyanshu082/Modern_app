595c53d45fb71ffa48b0ee1c046f3e8fc57d467d
priyanshu082 <priyanshusingh216@gmail.com>
2023-03-18 13:44:10 +0530

    billing sectio is complete

--- modern_app/src/components/Billing.js
+++ modern_app/src/components/Billing.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { apple, bill, google, } from '../assets'
+import styles, { layout } from '../style'
 
-const Billing = () => {
-  return (
-    <div>Billing</div>
-  )
-}
+const Billing = () => (
+  <section id='product' className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img src={bill} alt="bill" className='w-[100%] h-[100%] relative z-[5]' />
+      <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient' />
+      <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient' />
+    </div>
+    <div className={layout.sectionInfo}>
+      <h2 className={`${styles.heading2} pl-[35px]`}>Easily control your <br className='sm:block hidden' /> billing & invoicing.</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5 pl-[35px]`}>Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.</p>
+
+      <div className='flex flex-row flex-wrap sm:mt-10 mt-6 pl-[28px]'>
+        <img src={apple} alt="Apple_store" className='w-[128px] h-[42px] mr-5 cursor-pointer object-contain' />
+        <img src={google} alt="Google_store" className='w-[128px] h-[42px] cursor-pointer object-contain' />
+      </div>
+    </div>
+  </section>
+)
 
 export default Billing
